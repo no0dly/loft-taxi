c4eff2671c291444bc62b45de2177d699badf1d5
Vadim Dermanovskiy <thenoodly@gmail.com>
2020-09-27 23:33:25 -0700

    feat: added private route
    added private route

---
+++ src/PrivateRoute.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import authSelector from "./selectors/auth";
+
+export const PrivateRoute = ({ component: Component, isLoggedIn, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isLoggedIn ? <Component {...props} /> : <Redirect to="/" />
+    }
+  />
+);
+
+export default connect(authSelector)(PrivateRoute);
