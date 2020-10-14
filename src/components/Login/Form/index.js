ba5bf1754c6a90ff285cd83e37291cf379335818
Vadim Dermanovskiy <thenoodly@gmail.com>
2020-10-13 21:59:35 -0700

    feat: added react hook form

---
+++ src/components/Login/Form/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Typography, Link, TextField, Grid, Button } from "@material-ui/core";
+
+import { pageUrls } from "../../../utils/constants";
+
+import { useForm, Controller } from "react-hook-form";
+import * as actions from "../../../redux/actions";
+import { useDispatch } from "react-redux";
+
+export function LoginForm() {
+  const dispatch = useDispatch();
+  const { control, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    dispatch(actions.loginRequest(data));
+  };
+  return (
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <div className="title">
+        <div className="title__header">
+          <Typography variant="h4">Войти</Typography>
+        </div>
+        <div className="title__subtext">
+          <Typography variant="body1">
+            Новый пользователь?{" "}
+            <Link to={pageUrls.REGISTRATION} component={NavLink}>
+              Зарегистрируйтесь
+            </Link>
+          </Typography>
+        </div>
+      </div>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <div className="form__field">
+            <Controller
+              label="Имя пользователя"
+              type="email"
+              fullWidth
+              name="email"
+              required
+              as={TextField}
+              control={control}
+              defaultValue=""
+            />
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="form__field">
+            <Controller
+              type="password"
+              label="Пароль"
+              fullWidth
+              name="password"
+              required
+              as={TextField}
+              control={control}
+              defaultValue=""
+            />
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="form__field">
+            <Button type="submit" variant="contained" color="primary">
+              Войти
+            </Button>
+          </div>
+        </Grid>
+      </Grid>
+    </form>
+  );
+}
+
+export default LoginForm;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,16 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { func, bool } from "prop-types";
 import { Logo } from "loft-taxi-mui-theme";
-import { NavLink, useHistory } from "react-router-dom";
-import {
-  Typography,
-  Link,
-  TextField,
-  Paper,
-  Grid,
-  Button,
-} from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import LoginForm from "../../components/Login/Form";
+import { Paper } from "@material-ui/core";
 
 import "./Login.css";
 
@@ -20,18 +14,13 @@ import authSelector from "../../selectors/auth";
 import * as actions from "../../redux/actions";
 
 const proopTypes = {
-  loginRequest: func.isRequired,
   resetLoginStatus: func.isRequired,
   isLoaded: bool.isRequired,
   error: bool.isRequired,
 };
 
-export function Login({ loginRequest, resetLoginStatus, isLoaded, error }) {
+export function Login({ resetLoginStatus, isLoaded, error }) {
   const history = useHistory();
-  const [formFields, setFields] = useState({
-    email: "",
-    password: "",
-  });
 
   useEffect(() => {
     if (isLoaded && !error) {
@@ -44,18 +33,6 @@ export function Login({ loginRequest, resetLoginStatus, isLoaded, error }) {
     };
   }, [isLoaded, error]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const onChange = (e) => {
-    setFields({
-      ...formFields,
-      [e.target.name]: e.target.value,
-    });
-  };
-  const login = (e) => {
-    e.preventDefault();
-
-    loginRequest(formFields);
-  };
-
   return (
     <div className="login" data-testid="login">
       <div className="login__container">
@@ -63,58 +40,7 @@ export function Login({ loginRequest, resetLoginStatus, isLoaded, error }) {
           <Logo white animated />
         </div>
         <Paper className="login__form">
-          <form className="form" onSubmit={login} autoComplete="off">
-            <div className="title">
-              <div className="title__header">
-                <Typography variant="h4">Войти</Typography>
-              </div>
-              <div className="title__subtext">
-                <Typography variant="body1">
-                  Новый пользователь?{" "}
-                  <Link to={pageUrls.REGISTRATION} component={NavLink}>
-                    Зарегистрируйтесь
-                  </Link>
-                </Typography>
-              </div>
-            </div>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <div className="form__field">
-                  <TextField
-                    id="email"
-                    label="Имя пользователя"
-                    type="email"
-                    fullWidth
-                    name="email"
-                    required
-                    value={formFields.email}
-                    onChange={onChange}
-                  />
-                </div>
-              </Grid>
-              <Grid item xs={12}>
-                <div className="form__field">
-                  <TextField
-                    id="password"
-                    type="password"
-                    label="Пароль"
-                    fullWidth
-                    name="password"
-                    required
-                    value={formFields.password}
-                    onChange={onChange}
-                  />
-                </div>
-              </Grid>
-              <Grid item xs={12}>
-                <div className="form__field">
-                  <Button type="submit" variant="contained" color="primary">
-                    Войти
-                  </Button>
-                </div>
-              </Grid>
-            </Grid>
-          </form>
+          <LoginForm />
         </Paper>
       </div>
     </div>
